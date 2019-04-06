845a8a188b5c2bd8419291f187740e9219a8b570
devyboy <dev@udel.edu>
2019-04-05 20:31:33 -0400

    added twitter support

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# ipe.py: Instagram Picture Extractor
-Extracts images from Instagram posts. Downloads the images to the directory that the script is running from so keep that in mind. Also for some reason Instagram likes to have 3 copies of each image in varying resolutions which is why there are seemingly 3 copies of each.
+# tipe.py: Twitter and Instagram Picture Extractor
+Extracts images from Twitter and Instagram posts. Downloads the images to the directory that the script is running from so keep that in mind. Also for some reason Instagram likes to have 3 copies of each image in varying resolutions which is why there are 3 copies of each.
 # Usage
-Pretty simple: `python ipe.py https://www.instagram.com/p/BusBK6ahZ5Z/`
+Pretty simple: `python tipe.py https://www.instagram.com/p/BusBK6ahZ5Z/`
+or
+`python tipe.py https://twitter.com/BarackObama/status/1095882980839616512/`
---
+++ tipe.py
@@ -0,0 +1,48 @@
+import regex, re, sys, requests
+from bs4 import BeautifulSoup
+
+
+def downloadImages(imglist):
+    print("Downloading images to " + sys.path[0] + "...")
+    for image in imglist:
+        with open("Instagram_IMG_" + str(imglist.index(image)) + ".jpg", 'wb') as handle:
+            response = requests.get(image, stream=True)
+            if not response.ok:
+                print response
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+    print("Done")
+
+
+def start():
+
+    soup = BeautifulSoup(requests.get(sys.argv[1]).text, "html.parser")
+    imglist = []
+
+    if "twitter.com" in sys.argv[1]:
+        if "Sorry, that page doesn't exist" in soup:
+            quit("Error: Bad URL")
+        div = soup.find("div", {"class": "AdaptiveMedia"})
+        for img in div.find_all("img"):
+            if not img in imglist:
+                imglist.append(img["src"])
+        downloadImages(imglist)
+    elif "instagram.com" in sys.argv[1]:
+        if "Page Not Found" in soup:
+            quit("Error: Bad URL")
+        pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
+        print("Searching for images at: " + sys.argv[1])
+        for brackets in pattern.findall(str(soup)):
+            if "src" in brackets:
+                for match in re.finditer("src", brackets):
+                    begindex = match.end() + 3
+                    endex = brackets[begindex:-1].find("\"")
+                    imglist.append(brackets[begindex:begindex + endex])
+        downloadImages(imglist)
+    else:
+        print("Error: Please enter a Twitter or Instagram post")
+
+if __name__ == "__main__":
+    start()
